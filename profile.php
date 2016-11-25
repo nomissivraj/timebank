a34c65a8bac19be3ac3dacd6d41c7205b7e7bd6f
Simon Jarvis <Simon Jarvis>
2016-11-25 14:35:27 +0000

    i

--- profile.php
+++ profile.php
@@ -56,6 +56,10 @@
                                                     <td>PHONE: </td>
                                                     <td class="dynamic">'.$_SESSION['phone'].'</td>
                                                 </tr>
+                                                 <tr>
+                                                    <td>PHONE: </td>
+                                                    <td class="dynamic">'.$_SESSION['phone'].'</td>
+                                                </tr>
                                             </table>
                                         </div>
                                     </div>
